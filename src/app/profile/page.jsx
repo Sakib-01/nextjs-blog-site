1fc2d96b81cfda3c26b9f212badd062f202db943
Sakib-01 <syedsakibulhaque2@gmail.com>
2025-01-23 11:39:29 +0600

    make profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { redirect } from "next/navigation";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const profilepage = () => {
+const ProfilePage = async () => {
+  const { getUser, isAuthenticated } = getKindeServerSession();
+
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) {
+    redirect("/api/auth/login");
+  }
+  console.log(getUser);
+  const user = await getUser();
   return (
     <div>
-      <h2>this is profile page</h2>
+      <h2 className="text-3xl">
+        Welcome to your profile{" "}
+        <span>
+          {user?.given_name} {user?.family_name}
+        </span>
+      </h2>
     </div>
   );
 };
 
-export default profilepage;
+export default ProfilePage;
