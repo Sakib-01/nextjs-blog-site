39d530905d13e0e02365f33360d67d67113ffdcf
Sakib-01 <syedsakibulhaque2@gmail.com>
2025-01-23 20:05:00 +0600

    middle

--- middleware.js
+++ middleware.js
@@ -1,20 +1,20 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
-import { NextResponse } from "next/server";
-// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"; // Use edge runtime for Vercel
+// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server";
+// import { NextResponse } from "next/server";
+// // import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"; // Use edge runtime for Vercel
 
-export async function middleware(req) {
-  // const { isAuthenticated } = getKindeServerSession();
-  const { getUser } = getKindeServerSession();
-  const { isAuthenticated } = getKindeServerSession();
-  const isUserAuthenticated = await isAuthenticated();
+// export async function middleware(req) {
+//   // const { isAuthenticated } = getKindeServerSession();
+//   const { getUser } = getKindeServerSession();
+//   const { isAuthenticated } = getKindeServerSession();
+//   const isUserAuthenticated = await isAuthenticated();
 
-  if (!isUserAuthenticated) {
-    return NextResponse.redirect(new URL("/api/auth/login", req.url));
-  }
+//   if (!isUserAuthenticated) {
+//     return NextResponse.redirect(new URL("/api/auth/login", req.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: "/profile", // Protects all routes under /profile
-};
+// export const config = {
+//   matcher: "/profile", // Protects all routes under /profile
+// };
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-// import { redirect } from "next/navigation";
-// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const ProfilePage = async () => {
-  // const { getUser, isAuthenticated } = getKindeServerSession();
+  const { getUser, isAuthenticated } = getKindeServerSession();
 
-  // const user = await getUser();
-  // const isUserAuthenticated = await isAuthenticated();
-  // if (!isUserAuthenticated) {
-  //   redirect("/api/auth/login");
-  // }
-  // console.log(getUser);
+  const user = await getUser();
+
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) {
+    redirect("/api/auth/login");
+  }
+  console.log(getUser);
   return (
     <div>
       <h2 className="text-3xl">
