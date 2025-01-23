5b1b741bde47699bd26983241c2459f63cb0bdda
Sakib-01 <syedsakibulhaque2@gmail.com>
2025-01-23 21:03:20 +0600

    provider

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import { redirect } from "next/navigation";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+// import React from "react";
+// import { redirect } from "next/navigation";
+// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-const ProfilePage = async () => {
-  const { getUser, isAuthenticated } = getKindeServerSession();
+// const ProfilePage = async () => {
+//   const { getUser, isAuthenticated } = getKindeServerSession();
 
-  const user = await getUser();
+//   const user = await getUser();
 
-  const isUserAuthenticated = await isAuthenticated();
-  if (!isUserAuthenticated) {
-    redirect("/api/auth/login");
-  }
-  console.log(getUser);
-  return (
-    <div>
-      <h2 className="text-3xl">
-        <p>Welcome to your profile </p>
-        {/* <span>
-          Mr {user?.given_name} {user?.family_name}
-        </span> */}
-      </h2>
-    </div>
-  );
-};
+//   const isUserAuthenticated = await isAuthenticated();
+//   if (!isUserAuthenticated) {
+//     redirect("/api/auth/login");
+//   }
+//   console.log(getUser);
+//   return (
+//     <div>
+//       <h2 className="text-3xl">
+//         <p>Welcome to your profile </p>
+//         {/* <span>
+//           Mr {user?.given_name} {user?.family_name}
+//         </span> */}
+//       </h2>
+//     </div>
+//   );
+// };
 
-export default ProfilePage;
+// export default ProfilePage;
 
 // function app() {
 //   return <div>Welcome to your profile</div>;
@@ -43,3 +43,25 @@ export default ProfilePage;
 //     </div>
 //   );
 // }
+
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export default async function Profile() {
+  const { isAuthenticated, getUser } = getKindeServerSession();
+
+  const user = await getUser();
+
+  return (await isAuthenticated()) ? (
+    <section className="container mx-auto min-h-screen">
+      <p className="text-3xl font-semibold">
+        hey {user?.family_name} {user?.given_name}👋
+      </p>
+    </section>
+  ) : (
+    <div className="container mx-auto min-h-screen text-2xl">
+      This page is protected, please{" "}
+      <LoginLink className="font-bold underline">Login</LoginLink> to view it
+    </div>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,55 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import Link from "next/link";
-import React from "react";
+// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+// import Link from "next/link";
+// import React from "react";
 
+import Link from "next/link";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 const Navbar = async () => {
+  const { isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+
   const { getUser } = getKindeServerSession();
   const user = await getUser();
   console.log(user);
+  const links = (
+    <>
+      <li>
+        {" "}
+        <Link href={"/"}>Blog</Link>
+      </li>
+      <li>
+        {" "}
+        <Link href={"/profile"}>Profile</Link>{" "}
+      </li>
+      {isUserAuthenticated ? (
+        <>
+          <li>
+            <LogoutLink>Log out</LogoutLink>{" "}
+          </li>
+        </>
+      ) : (
+        <>
+          <li>
+            {" "}
+            <LoginLink>Login</LoginLink>
+          </li>
+          <li>
+            {" "}
+            <RegisterLink>Registerr</RegisterLink>{" "}
+          </li>
+        </>
+      )}
+    </>
+  );
   return (
     <div className="container mx-auto my-10">
       <div>
-        <ul className="flex gap-6 ">
-          <li>
-            <Link href={"/"}>Home </Link>
-          </li>
-
-          {/* <li>
-            <Link href={"/blogs"}>Blogs </Link>
-          </li> */}
-          <li>
-            <Link href={"/profile"}>Profile </Link>
-          </li>
-          {user ? (
-            <>
-              <h2>{user?.given_name}</h2>
-              <li>
-                <Link href={"/api/auth/logout"}>logout</Link>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link href={"/api/auth/login"}>login </Link>
-              </li>
-              <li>
-                <Link href={"/api/auth/register"}>Signup </Link>
-              </li>
-            </>
-          )}
-        </ul>
+        <ul className="flex gap-6 ">{links}</ul>
       </div>
     </div>
   );
