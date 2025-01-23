3cf59ca50cc7bb14a8ec121eb493d577c1f554c9
Sakib-01 <syedsakibulhaque2@gmail.com>
2025-01-23 13:06:38 +0600

    profile change

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -5,20 +5,28 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 const ProfilePage = async () => {
   const { getUser, isAuthenticated } = getKindeServerSession();
 
+  // Check if the user is authenticated
   const isUserAuthenticated = await isAuthenticated();
   if (!isUserAuthenticated) {
     redirect("/api/auth/login");
+    return null; // Prevent further rendering
   }
-  console.log(getUser);
+
+  // Fetch the user details
   const user = await getUser();
+
   return (
-    <div>
-      <h2 className="text-3xl">
-        Welcome to your profile{" "}
-        <span>
+    <div className="container mx-auto my-10">
+      <h2 className="text-3xl font-bold mb-4">Welcome to your profile!</h2>
+      <p className="text-xl">
+        Name:{" "}
+        <strong>
           {user?.given_name} {user?.family_name}
-        </span>
-      </h2>
+        </strong>
+      </p>
+      <p className="text-xl">
+        Email: <strong>{user?.email}</strong>
+      </p>
     </div>
   );
 };
