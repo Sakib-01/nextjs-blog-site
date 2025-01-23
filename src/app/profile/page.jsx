4aebac3b55b54651a972265fa9189254c3a71e1b
Sakib-01 <syedsakibulhaque2@gmail.com>
2025-01-23 11:14:24 +0600

    project navbar using kindeAuth

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const profilepage = () => {
+  return (
+    <div>
+      <h2>this is profile page</h2>
+    </div>
+  );
+};
+
+export default profilepage;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  console.log(user);
+  return (
+    <div className="container mx-auto my-10">
+      <div>
+        <ul className="flex gap-6 ">
+          <li>
+            <Link href={"/"}>Home </Link>
+          </li>
+
+          {/* <li>
+            <Link href={"/blogs"}>Blogs </Link>
+          </li> */}
+          <li>
+            <Link href={"/profile"}>Profile </Link>
+          </li>
+          {user ? (
+            <>
+              <li>
+                <Link href={"/api/auth/logout"}>logout</Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href={"/api/auth/login"}>login </Link>
+              </li>
+              <li>
+                <Link href={"/api/auth/register"}>Signup </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
