27b7cb919ac4b62aeac304db1b18da6d24d83f29
Sakib-01 <syedsakibulhaque2@gmail.com>
2025-01-23 11:27:32 +0600

    make blog page

--- src/app/blogs/[id]/page.jsx
+++ src/app/blogs/[id]/page.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+"use client";
 
-const blogpage = () => {
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const SingleBlog = () => {
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${id}`
+      );
+      if (!res.ok) throw new Error("Failed to fetch the blog post");
+      const data = await res.json();
+      setPost(data);
+    };
+
+    if (id) fetchPost();
+  }, [id]);
+
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h2>this is blog page</h2>
+      <h2>Single Blog</h2>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold mb-4">
+          <span className="underline">Blog title </span> : {post.title}
+        </h1>
+        <p> Description: {post.body}</p>
+      </div>
     </div>
   );
 };
 
-export default blogpage;
+export default SingleBlog;
