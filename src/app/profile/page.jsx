db4ad6ea6c7e93094ef6828010b2678c64f62245
Sakib-01 <syedsakibulhaque2@gmail.com>
2025-01-23 14:18:40 +0600

    profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-// import { redirect } from "next/navigation";
-// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const ProfilePage = async () => {
-  // const { getUser, isAuthenticated } = getKindeServerSession();
+  const { getUser, isAuthenticated } = getKindeServerSession();
 
-  // const isUserAuthenticated = await isAuthenticated();
-  // if (!isUserAuthenticated) {
-  //   redirect("/api/auth/login");
-  // }
-  // console.log(getUser);
-  // const user = await getUser();
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) {
+    redirect("/api/auth/login");
+  }
+  console.log(getUser);
+  const user = await getUser();
   return (
     <div>
       <h2 className="text-3xl">
         Welcome to your profile{" "}
-        {/* <span>
+        <span>
           Mr {user?.given_name} {user?.family_name}
-        </span> */}
+        </span>
       </h2>
     </div>
   );
