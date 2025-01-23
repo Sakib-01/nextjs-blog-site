9c59a729416575f48f039551f0c4080275169709
Sakib-01 <syedsakibulhaque2@gmail.com>
2025-01-23 14:02:59 +0600

    add middleware

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,32 +1,24 @@
 import React from "react";
-import { redirect } from "next/navigation";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+// import { redirect } from "next/navigation";
+// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const ProfilePage = async () => {
-  const { getUser, isAuthenticated } = getKindeServerSession();
+  // const { getUser, isAuthenticated } = getKindeServerSession();
 
-  // Check if the user is authenticated
-  const isUserAuthenticated = await isAuthenticated();
-  if (!isUserAuthenticated) {
-    redirect("/api/auth/login");
-    return null; // Prevent further rendering
-  }
-
-  // Fetch the user details
-  const user = await getUser();
-
+  // const isUserAuthenticated = await isAuthenticated();
+  // if (!isUserAuthenticated) {
+  //   redirect("/api/auth/login");
+  // }
+  // console.log(getUser);
+  // const user = await getUser();
   return (
-    <div className="container mx-auto my-10">
-      <h2 className="text-3xl font-bold mb-4">Welcome to your profile!</h2>
-      <p className="text-xl">
-        Name:{" "}
-        <strong>
-          {user?.given_name} {user?.family_name}
-        </strong>
-      </p>
-      <p className="text-xl">
-        Email: <strong>{user?.email}</strong>
-      </p>
+    <div>
+      <h2 className="text-3xl">
+        Welcome to your profile{" "}
+        {/* <span>
+          Mr {user?.given_name} {user?.family_name}
+        </span> */}
+      </h2>
     </div>
   );
 };
